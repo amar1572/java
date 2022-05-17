f3045c446d3e814e3fcf2eb6b9ea73e79b2608be
Amar Hanmant Phate <89267186+amar1572@users.noreply.github.com>
2022-05-17 16:40:47 +0530

    Add files via upload

---
+++ Practical/Increment.java
@@ -0,0 +1,18 @@
+package IncrementOperations;
+
+public class Increment {
+
+	public static void main(String[] args)
+	{
+		int p = 5;
+		int q = 2;
+		int r;
+		int s;
+		r = ++q;
+		s = p++;
+		r++;
+		System.out.println("p=" +p+ "\tq ="+q+ "\tr=" +r+ "\ts="+s);
+
+	}
+
+}
