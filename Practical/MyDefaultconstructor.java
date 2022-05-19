98a37e9ec66426255ef981b8ef2803e605f79102
Amar Hanmant Phate <89267186+amar1572@users.noreply.github.com>
2022-05-19 18:32:08 +0530

    Add files via upload

---
+++ Practical/MyDefaultconstructor.java
@@ -0,0 +1,13 @@
+package construct;
+
+public class MyDefaultconstructor<myDefaultconstructor> {
+
+		public MyDefaultconstructor(){
+			System.out.println("This is Deafault Constructor");
+			}
+public static void main(String args[]){
+
+MyDefaultconstructor mdc = new MyDefaultconstructor();
+	}
+
+}
