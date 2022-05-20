5f63dec4913631ad964f22ebfaf7bc3b6f966e0e
Amar Hanmant Phate <89267186+amar1572@users.noreply.github.com>
2022-05-20 15:54:26 +0530

    Add files via upload

---
+++ Practical/ConstructorOverloadingExample2.java
@@ -0,0 +1,25 @@
+package overloading2;
+
+public class Student {
+		int id;
+		String name;
+		int age;
+		Student(int i,String n){
+		id = i;
+		name = n;
+		}
+		Student(int i,String n,int a){
+		id = i;
+		name = n;
+		age=a;
+		}
+		void display(){System.out.println(id+" "+name+" "+age);}
+
+	public static void main(String[] args) {
+		Student s1 = new Student(16,"Ram",42);
+		Student s2 = new Student(17,"Ratan",27);
+		s1.display();
+		s2.display();
+	}
+	}
+
