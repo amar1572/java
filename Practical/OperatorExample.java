41a6bb4c71cacdc8ffa4cc805c97a546aac36388
Amar Hanmant Phate <89267186+amar1572@users.noreply.github.com>
2022-05-20 16:36:44 +0530

    Add files via upload

---
+++ Practical/OperatorExample.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		
+			int a=18;
+			int b=7;
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			}
+	}
+
---
+++ Practical/OperatorExamples2.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class OperatorExamples {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=3;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+
+	}
+
+}
