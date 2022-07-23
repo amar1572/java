9e2c8ce25d04afca6133b5f39ccd8d5d872b0492
Amar Hanmant Phate <89267186+amar1572@users.noreply.github.com>
2022-07-23 22:06:04 +0530

    Add files via upload

--- Practical/Increment.java
+++ Practical/Increment.java
@@ -1,17 +1,16 @@
-package IncrementOperations;
+package com;
 
 public class Increment {
 
-	public static void main(String[] args)
-	{
-		int p = 5;
-		int q = 2;
+	public static void main(String[] args) {
+		int p=10;
+		int q=20;
 		int r;
 		int s;
-		r = ++q;
-		s = p++;
+		r=++q;
+		s=p++;
 		r++;
-		System.out.println("p=" +p+ "\tq ="+q+ "\tr=" +r+ "\ts="+s);
+		System.out.println("p= " +p +" q= " +q +" r= " +r +" s= " +s);
 
 	}
 
---
+++ Practical/Scanner1.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+	
+	static int add(int num1,int num2) {
+		int sum =num1+num2;
+		return sum;		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int n1=sc.nextInt();
+		System.out.println("Enter second number:");
+		int n2=sc.nextInt();
+		
+		int result=add(n1,n2);
+		System.out.println("Addition :" + result);
+
+	}
+
+}
